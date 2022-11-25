c4e75de558411991215eb151ffddf87911d2767c
João marcos <112022434+Nesrux@users.noreply.github.com>
2022-11-25 19:49:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    },500)
+}
+
+const loop = setInterval(() =>{
+    const pipePosition = pipe.offsetLeft;
+    const marioPostion = +  window.getComputedStyle(mario).bottom.replace('px', '');
+
+    
+    console.log(loop);
+
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPostion < 80){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        pipe.style.animation = 'none';
+        pipe.style.bottom = `${marioPostion}px`;
+
+        mario.src="./assets/game-over.png";
+        mario.style.width= '75px';
+        mario.style.marginLeft = '50px';
+
+
+        clearInterval(loop);
+        location.reload();
+    }
+}, 10)
+
+document.addEventListener('keydown', jump);
